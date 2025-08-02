e421822e920a28977b8ece959cda1c876962c38d
swag100 <ti.n.tripp@baypath.net>
2025-08-01 23:04:42 -0400

    EVERYTHING TEST

--- src/main.cpp
+++ src/main.cpp
@@ -1,387 +1,229 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2024
-and may not be redistributed without written permission.*/
-
-//Using SDL, SDL_image, SDL_ttf, standard IO, math, and strings
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <stdio.h>
+#include <iostream>
 #include <string>
-#include <cmath>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
 
-//Texture wrapper class
-class LTexture
-{
-public:
-	//Initializes variables
-	LTexture();
+// You must include the command line parameters for your main function to be recognized by SDL
+int main(int argc, char** args) {
 
-	//Deallocates memory
-	~LTexture();
+	// Pointers to our window and surface
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 
-	//Loads image at specified path
-	bool loadFromFile(std::string path);
+	// Initialize SDL. SDL_Init will return -1 if it fails.
+	int imgFlags = IMG_INIT_PNG;
+	if (!(IMG_Init(imgFlags) & imgFlags)){
 
-	//Creates image from font string
-	bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
-
-	//Deallocates texture
-	void free();
-
-	//Set color modulation
-	void setColor(Uint8 red, Uint8 green, Uint8 blue);
-
-	//Set blending
-	void setBlendMode(SDL_BlendMode blending);
-
-	//Set alpha modulation
-	void setAlpha(Uint8 alpha);
-
-	//Renders texture at given point
-	void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
-
-	//Gets image dimensions
-	int getWidth();
-	int getHeight();
-
-private:
-	//The actual hardware texture
-	SDL_Texture* mTexture;
-
-	//Image dimensions
-	int mWidth;
-	int mHeight;
-};
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
-
-//Globally used font
-TTF_Font* gFont = NULL;
-
-//Rendered texture
-LTexture gTextTexture;
-
-
-LTexture::LTexture()
-{
-	//Initialize
-	mTexture = NULL;
-	mWidth = 0;
-	mHeight = 0;
-}
-
-LTexture::~LTexture()
-{
-	//Deallocate
-	free();
-}
-
-bool LTexture::loadFromFile(std::string path)
-{
-	//Get rid of preexisting texture
-	free();
-
-	//The final texture
-	SDL_Texture* newTexture = NULL;
-
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-	if (loadedSurface == NULL)
-	{
-		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+		printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+		system("pause");
+		return 1;
 	}
-	else
+	if (TTF_Init() < 0) {
+		std::cout << "Error initializing SDL_ttf: " << TTF_GetError() << std::endl;
+		system("pause");
+		return 1;
+	}
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
+		system("pause");
+		return 1;
+	}
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
 	{
-		//Color key image
-		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
-
-		//Create texture from surface pixels
-		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-		if (newTexture == NULL)
-		{
-			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
-		}
-		else
-		{
-			//Get image dimensions
-			mWidth = loadedSurface->w;
-			mHeight = loadedSurface->h;
-		}
-
-		//Get rid of old loaded surface
-		SDL_FreeSurface(loadedSurface);
+		printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+		system("pause");
+		return 1;
 	}
 
-	//Return success
-	mTexture = newTexture;
-	return mTexture != NULL;
-}
+	// Create our window
 
-bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
-{
-	//Get rid of preexisting texture
-	free();
-
-	//Render text surface
-	SDL_Surface* textSurface = TTF_RenderText_Solid(gFont, textureText.c_str(), textColor);
-	if (textSurface == NULL)
-	{
-		printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
-	}
-	else
-	{
-		//Create texture from surface pixels
-		mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
-		if (mTexture == NULL)
-		{
-			printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get image dimensions
-			mWidth = textSurface->w;
-			mHeight = textSurface->h;
-		}
-
-		//Get rid of old surface
-		SDL_FreeSurface(textSurface);
+	int result = SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer);
+	if (result != 0) {
+		std::cout << "Failed to create window and renderer: " << SDL_GetError() << std::endl;
 	}
 
-	//Return success
-	return mTexture != NULL;
-}
+	SDL_Texture* screen = SDL_CreateTexture(renderer, 
+		SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET,
+		SCREEN_WIDTH, SCREEN_HEIGHT
+	);
 
-void LTexture::free()
-{
-	//Free texture if it exists
-	if (mTexture != NULL)
-	{
-		SDL_DestroyTexture(mTexture);
-		mTexture = NULL;
-		mWidth = 0;
-		mHeight = 0;
+	//THIS CREATES MIXELS AAAAAAAHHHH
+	//SDL_RenderSetLogicalSize(renderer, 320, 240);
+
+	//test fonts
+
+	TTF_Font* font;
+	font = TTF_OpenFont("res/font/Zont.ttf", 24);
+	if (!font) {
+		std::cout << "Failed to load font: " << TTF_GetError() << std::endl;
 	}
-}
-
-void LTexture::setColor(Uint8 red, Uint8 green, Uint8 blue)
-{
-	//Modulate texture rgb
-	SDL_SetTextureColorMod(mTexture, red, green, blue);
-}
-
-void LTexture::setBlendMode(SDL_BlendMode blending)
-{
-	//Set blending function
-	SDL_SetTextureBlendMode(mTexture, blending);
-}
-
-void LTexture::setAlpha(Uint8 alpha)
-{
-	//Modulate texture alpha
-	SDL_SetTextureAlphaMod(mTexture, alpha);
-}
-
-void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
-{
-	//Set rendering space and render to screen
-	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
-
-	//Set clip rendering dimensions
-	if (clip != NULL)
-	{
-		renderQuad.w = clip->w;
-		renderQuad.h = clip->h;
+	SDL_Surface* testTextSurf;
+	SDL_Color color = { 255, 255, 255 };
+	testTextSurf = TTF_RenderText_Solid(font, "Hello World!", color);
+	if (!testTextSurf) {
+		std::cout << "Failed to render text: " << TTF_GetError() << std::endl;
 	}
+	SDL_Texture* text_texture;
+	text_texture = SDL_CreateTextureFromSurface(renderer, testTextSurf);
 
-	//Render to screen
-	SDL_RenderCopyEx(gRenderer, mTexture, clip, &renderQuad, angle, center, flip);
-}
+	//player image
 
-int LTexture::getWidth()
-{
-	return mWidth;
-}
+	SDL_Surface* image = IMG_Load("res/images/tn_flag.png");
 
-int LTexture::getHeight()
-{
-	return mHeight;
-}
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
 
-bool init()
-{
-	//Initialization flag
-	bool success = true;
+	SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+	SDL_SetTextureAlphaMod(texture, 128);
 
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
-		{
-			printf("Warning: Linear texture filtering not enabled!");
-		}
+	//get info about it
+	int w, h, access;
+	unsigned int format;
+	SDL_QueryTexture(texture, &format, &access, &w, &h);
 
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL)
-		{
-			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
-			success = false;
-		}
-		else
-		{
-			//Create vsynced renderer for window
-			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-			if (gRenderer == NULL)
-			{
-				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	int playerRotation = 0;
 
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if (!(IMG_Init(imgFlags) & imgFlags))
-				{
-					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-					success = false;
-				}
+	SDL_FreeSurface(image);
+	image = NULL;
 
-				//Initialize SDL_ttf
-				if (TTF_Init() == -1)
-				{
-					printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
-					success = false;
-				}
-			}
+	//SFX
+	Mix_Chunk* blips[4];
+	for (int i= 0; i < 4; ++i) {
+		std::string filename = "res/sounds/blip" + std::to_string(i + 1) + ".wav";
+		blips[i] = Mix_LoadWAV(filename.c_str());
+		if (!blips[i]) {
+			std::cout << "Failed to load sound effect: " << Mix_GetError() << std::endl;
+			system("pause");
+			return 1;
 		}
 	}
 
-	return success;
-}
+	// Get the surface from the window
+	//winSurface = SDL_GetWindowSurface(window);
 
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
+	/*/ Make sure getting the surface succeeded
+	if (!winSurface) {
+		std::cout << "Error getting surface: " << SDL_GetError() << std::endl;
+		system("pause");
+		// End the program
+		return 1;
+	}*/
 
-	//Open the font
-	gFont = TTF_OpenFont("res/font/lazy.ttf", 28);
-	if (gFont == NULL)
-	{
-		printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
-		success = false;
-	}
-	else
-	{
-		//Render text
-		SDL_Color textColor = { 0, 0, 0 };
-		if (!gTextTexture.loadFromRenderedText("The quick brown fox jumps over the lazy dog", textColor))
-		{
-			printf("Failed to render text texture!\n");
-			success = false;
-		}
-	}
+	SDL_Event event;
+	bool running = true;
 
-	return success;
-}
+	SDL_Rect player = {100, 100, 64, 64};
+	player.x = 100; // Set the x position of the player
 
-void close()
-{
-	//Free loaded images
-	gTextTexture.free();
+	// Main loop
+	while (running) {
+		// Event loop
+		SDL_SetRenderTarget(renderer, screen);
 
-	//Free global font
-	TTF_CloseFont(gFont);
-	gFont = NULL;
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		// Fill the window with a white rectangle
+		SDL_RenderClear(renderer);
+		//SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 0, 0, 0));
 
-	//Destroy window	
-	SDL_DestroyRenderer(gRenderer);
-	SDL_DestroyWindow(gWindow);
-	gWindow = NULL;
-	gRenderer = NULL;
+		while ( SDL_PollEvent( &event ) ) {
+			// check event type
+			switch ( event.type ) {
+				case SDL_QUIT:
+					// shut down
+					running = false;
 
-	//Quit SDL subsystems
-	TTF_Quit();
-	IMG_Quit();
-	SDL_Quit();
-}
-
-int main(int argc, char* args[])
-{
-	//Start up SDL and create window
-	if (!init())
-	{
-		printf("Failed to initialize!\n");
-	}
-	else
-	{
-		//Load media
-		if (!loadMedia())
-		{
-			printf("Failed to load media!\n");
-		}
-		else
-		{
-			//Main loop flag
-			bool quit = false;
-
-			//Event handler
-			SDL_Event e;
-
-			//While application is running
-			while (!quit)
-			{
-				//Handle events on queue
-				while (SDL_PollEvent(&e) != 0)
-				{
-					//User requests quit
-					if (e.type == SDL_QUIT)
-					{
-						quit = true;
+					break;
+				case SDL_KEYDOWN:
+					// test keycode
+					switch ( event.key.keysym.sym ) {
+						case SDLK_LEFT:
+							std::cout << "A key pressed" << std::endl;
+							playerRotation -= 10; // Move player up
+							Mix_PlayChannel(-1, blips[0], 0); // Play sound effect
+							break;
+						case SDLK_RIGHT:
+							std::cout << "D key pressed" << std::endl;
+							Mix_PlayChannel(-1, blips[1], 0); // Play sound effect
+							playerRotation += 10; // Move player up
+							break;
+						case SDLK_UP:
+							std::cout << "W key pressed" << std::endl;
+							player.y -= 10; // Move player up
+							Mix_PlayChannel(-1, blips[2], 0); // Play sound effect
+							break;
+						case SDLK_DOWN:
+							std::cout << "S key pressed" << std::endl;
+							player.y += 10; // Move player up
+							Mix_PlayChannel(-1, blips[3], 0); // Play sound effect
+							break;
+						// etc
 					}
-				}
 
-				//Clear screen
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-				SDL_RenderClear(gRenderer);
-
-				//Render current frame
-				gTextTexture.render((SCREEN_WIDTH - gTextTexture.getWidth()) / 2, (SCREEN_HEIGHT - gTextTexture.getHeight()) / 2);
-
-				//Update screen
-				SDL_RenderPresent(gRenderer);
+					break;
 			}
+		}
+
+		// Fill the window with a white rectangle
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderFillRect(renderer, &player); // Draw a filled rectangle
+
+		SDL_Rect newRect = { player.x - w + (player.w / 2), player.y - h + (player.h / 2), w * 2, h * 2 };
+
+		SDL_Point rotPoint;
+		rotPoint.x = w;
+		rotPoint.y = h;
+
+		SDL_RenderCopyEx(renderer, texture, NULL, &newRect, (playerRotation % 360), &rotPoint, SDL_FLIP_HORIZONTAL);
+		//SDL_RenderCopy(renderer, texture, NULL, &newRect);
+		//SDL_FillRect(winSurface, &player, SDL_MapRGB(winSurface->format, 0, 255, 0));
+
+		SDL_Rect dest = { 0, 0, testTextSurf->w, testTextSurf->h };
+		SDL_RenderCopy(renderer, text_texture, NULL, &dest);
+
+		//render target to window
+		SDL_SetRenderTarget(renderer, NULL);
+
+		//
+
+		SDL_RenderCopy(renderer, screen, NULL, NULL);
+
+		// Update window
+		SDL_RenderPresent(renderer);
+
+		// Wait before next frame
+		SDL_Delay(15);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	SDL_FreeSurface(testTextSurf);
+	SDL_DestroyTexture(text_texture);
+	SDL_DestroyTexture(screen);
+	screen = NULL;
+	SDL_DestroyTexture(texture);
+	texture = NULL;
+
+	renderer = NULL;
+	window = NULL;
+
+	for (int i = 0; i < 4; ++i) {
+		if (blips[i]) {
+			Mix_FreeChunk(blips[i]);
+			blips[i] = NULL;
 		}
 	}
 
-	//Free resources and close SDL
-	close();
+	// Quit SDL
+	Mix_Quit();
+	IMG_Quit();
+	TTF_Quit();
+	SDL_Quit();
 
+	// End the program
 	return 0;
 }
